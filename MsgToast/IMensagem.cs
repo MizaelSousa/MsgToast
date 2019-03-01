6db5d9c34066db228ecf91bf1a9525227a6bd278
Mizael Sousa <aluno@MacBook-Pro-de-Aluno.local>
2019-02-28 20:14:40 -0400

    Clique longo e curto

---
+++ MsgToast/IMensagem.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MsgToast
+{
+    public interface IMensagem
+    {
+        void Longtime(string msg);
+        void Shorttime(string msg);
+    }
+}
---
+++ MsgToast/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.Widget;
+using Xamarin.Forms;
+
+namespace MsgToast
+{
+    public partial class MainPage : ContentPage , IMensagem
+    {
+        void cliqueCurto(object sender, System.EventArgs e)
+        {
+            Shorttime("Testando tempo curto");
+        }
+
+        void cliqueLongo(object sender, System.EventArgs e)
+        {
+            Longtime("Testando tempo longo");
+        }
+
+        public void Longtime(string msg)
+        {
+            Toast.MakeText(Android.App.Application.Context, msg, ToastLength.Long).Show();
+        }
+
+        public void Shorttime(string msg)
+        {
+            Toast.MakeText(Android.App.Application.Context, msg, ToastLength.Short).Show();
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
